663a1f2804323b2d29bf4ef91400bfc1ba1dea2f
mehmettalii <makusek1453@gmail.com>
2021-08-04 11:33:18 +0300

    API project

---
+++ Çalışma-9/profile.js
@@ -0,0 +1,22 @@
+class Profile{
+    constructor(){
+        this.clientid='',
+        this.clientsecret=''
+    }
+    async getProfile(username){
+        const profileResponse = await fetch
+        (`https://jsonplaceholder.typicode.com/users?username=${username}`);
+
+        const profile = await profileResponse.json();
+
+        const todoResponse = await fetch
+        (`https://jsonplaceholder.typicode.com/todos?userId=${profile[0].id}`)
+
+        const todo = await todoResponse.json();
+
+        return{
+            profile,
+            todo
+        }
+    }
+}
